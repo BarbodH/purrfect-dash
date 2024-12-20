b31e0491e48b668ed68201724f7f02a2c8aab452
BarbodH <habibi.k.barbod@gmail.com>
2024-12-20 03:24:28 -0500

    Move CollectibleCollection script to collectible prefab

--- Assets/Scripts/CollectibleCollision.cs
+++ Assets/Scripts/CollectibleCollision.cs
@@ -2,18 +2,9 @@ using UnityEngine;
 
 public class CollectibleCollision : MonoBehaviour
 {
-    private const string TagName = "Collectible";
-    private GameManager _gameManager;
-
-    private void Start()
-    {
-        _gameManager = GameManager.Instance;
-    }
-    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag(TagName)) return;
-        Destroy(other.gameObject);
-        _gameManager.IncrementCurrentScore();
+        Destroy(gameObject);
+        GameManager.Instance.IncrementCurrentScore();
     }
 }
