5cb500f6e20523536d684e5dd210b518fbbd9acc
BarbodH <habibi.k.barbod@gmail.com>
2024-12-20 15:56:40 -0500

    Add jump & attack SFX

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,10 +9,18 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform bottomPivot;
     [SerializeField] private float groundDistance = 0.2f;
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioClip jumpAudioClip;
+    [SerializeField] private AudioClip attackAudioClip;
+    private AudioManager _audioManager;
     private static readonly int AnimatorParamJump = Animator.StringToHash("Jump");
     private static readonly int AnimatorParamAttack = Animator.StringToHash("Attack");
     private bool _isGrounded;
     private bool _isJumping;
+
+    private void Start()
+    {
+        _audioManager = AudioManager.Instance;
+    }
 
     private void Update()
     {
@@ -50,12 +59,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Jump()
     {
+        _audioManager.PlaySoundEffect(jumpAudioClip);
         body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         _isJumping = false;
     }
     
     private void Attack() 
     {
+        _audioManager.PlaySoundEffect(attackAudioClip);
         animator.SetTrigger(AnimatorParamAttack);
     }
 }
